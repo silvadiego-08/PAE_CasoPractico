fa5df1bd891d2230592867c1e031cdc9938c5a18
diegoSilva <djsilvacubas@uamv.edu.ni>
2026-09-16 19:03:43 -0600

    add: login controller

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module uam.edu.ni.demo {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens uam.edu.ni.demo to javafx.fxml;
+    opens uam.edu.ni.demo.controller to javafx.fxml;
     exports uam.edu.ni.demo;
 }
--- src/main/java/uam/edu/ni/demo/RegistroApp.java
+++ src/main/java/uam/edu/ni/demo/RegistroApp.java
@@ -7,12 +7,14 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class RegistroApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                RegistroApp.class.getResource("/uam/edu/ni/demo/view/login-view.fxml")
+        );
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Inicio de sesión");
         stage.setScene(scene);
         stage.show();
     }
